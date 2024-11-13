22126e3646e8d41f2b5f141905edf1f6b50fab72
Urtix <es.perevalov@mail.ru>
2024-11-13 22:53:01 +0300

    test(Main): Add example of work
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- Program/src/main/kotlin/Main.kt
+++ Program/src/main/kotlin/Main.kt
@@ -3,26 +3,42 @@ const val SEED = 10
 const val AMBIT = 5
 
 fun main() {
-    val height = 4
-    val weight = 4
-    val amountCats = 4
-    val r0 = 0.0
-    val r1 = 10.0
+    val height = 5
+    val weight = 5
+    val amountCats = 7
+    val r0 = 1.0F
+    val r1 = 10.0F
 
     val cats = createCats(amountCats, height, weight)
     val catsMap = Map().createCatsMap(cats, height, weight)
 
-    UpdateStatus(catsMap, r0, r1)
+    UpdateStatus(catsMap, r0, r1).updateStatus()
 
     Print(Map().visualCatsMap(catsMap))
 
-    Map().moveCatsMap(cats, amountCats, height, weight)
-    val newCatsMap = Map().createCatsMap(cats, height, weight)
+    Map().moveCatsMap(catsMap, cats, amountCats, height, weight, r0, r1)
+    //val newCatsMap = Map().createCatsMap(cats, height, weight)
 
-    UpdateStatus(newCatsMap, r0, r1)
+    Print(catsMap)
+    Print(Map().visualCatsMap(catsMap))
 
-    Print(newCatsMap)
+    Map().moveCatsMap(catsMap, cats, amountCats, height, weight, r0, r1)
 
-    Print(Map().visualCatsMap(newCatsMap))
+    Print(catsMap)
+    Print(Map().visualCatsMap(catsMap))
+
+    Map().moveCatsMap(catsMap, cats, amountCats, height, weight, r0, r1)
+
+    Print(catsMap)
+    Print(Map().visualCatsMap(catsMap))
+
+    Map().moveCatsMap(catsMap, cats, amountCats, height, weight, r0, r1)
+
+    Print(catsMap)
+    Print(Map().visualCatsMap(catsMap))
+
+
+    //UpdateStatus(newCatsMap, r0, r1)
+    //Print(Map().visualCatsMap(newCatsMap))
 
 }
