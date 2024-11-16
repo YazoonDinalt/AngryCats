eff3dda1888570b01c3c7a355e6075799b6e50af
Urtix <es.perevalov@mail.ru>
2024-11-16 22:05:37 +0300

    feat: Add an adequate version of O(n^2)
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- Program/src/main/kotlin/Map.kt
+++ Program/src/main/kotlin/Map.kt
@@ -2,69 +2,19 @@ import kotlin.collections.Map
 import kotlin.random.Random
 
 class Map {
-    fun createCatsMap(cats: Array<Cat>, height: Int, weight: Int): Array<Array<CatInMap>> {
-        val catsMap = Array(weight) { Array(height) { CatInMap(0, Cat(0, 0, Status.None)) } }
+    fun visualCatsMap(cats: Array<Cat>, weight: Int, height: Int): Array<Array<String>> {
+        val visCatsMap = Array(weight) { Array(height) { "0" } }
 
-        for (cat in cats) {
-            if (catsMap[cat.x][cat.y].number != 0) {
-                catsMap[cat.x][cat.y].number++
-                catsMap[cat.x][cat.y].cat.status = Status.FIGHT
-                catsMap[cat.x][cat.y].cat = cat
-                cat.status = Status.FIGHT
-                println("Коты в x = ${cat.x} y = ${cat.y} дерутся")
-                continue
-            }
-
-            catsMap[cat.x][cat.y].number++
-            catsMap[cat.x][cat.y].cat = cat
-        }
-
-        for (y in 0 until height) {
-            for (x in 0 until weight) {
-                if (catsMap[x][y].number == 0) {
-                    catsMap[x][y].cat = Cat(x, y, Status.None)
-                }
-            }
-        }
-
-        return catsMap
-    }
-
-    fun visualCatsMap(catsMap: Array<Array<CatInMap>>): Array<Array<String>> {
-        val visCatsMap = Array(catsMap.size) { Array(catsMap[0].size) { "0" } }
-
-        for (i in catsMap.indices) {
-            for (j in 0 until catsMap[i].size) {
-                if (catsMap[i][j].number == 0) {
-                    continue
-                }
-
-                when (catsMap[i][j].cat.status) {
-                    Status.FIGHT -> visCatsMap[i][j] = "F"
-                    Status.WALK -> visCatsMap[i][j] = "W"
-                    Status.HISS -> visCatsMap[i][j] = "H"
-                    else -> println("WTF")
-                }
+        for (i in cats.indices) {
+            when (cats[i].status) {
+                Status.FIGHT -> visCatsMap[cats[i].x][cats[i].y] = "F"
+                Status.WALK -> visCatsMap[cats[i].x][cats[i].y] = "W"
+                Status.HISS -> visCatsMap[cats[i].x][cats[i].y] = "H"
+                else -> println("WTF")
             }
         }
 
         return visCatsMap
-    }
-
-    private fun mixingCats(cats: Array<Cat>, amountCats: Int): MutableList<Int> {
-        val random = Random(SEED) // Создаем новый объект Random с заданным SEED
-        val idCats = mutableListOf<Int>()
-
-        while (idCats.size < amountCats) {
-            val randomIndex = random.nextInt(cats.size)
-
-            if (!idCats.contains(randomIndex)) {
-                idCats.add(randomIndex)
-            }
-        }
-
-        return idCats
-
     }
 
     private fun move(distance: Int): Int {
@@ -77,7 +27,7 @@ class Map {
 
         if (distance / AMBIT >= 2) {
             move = if (randomOp < 0.5) {
-                -Random.nextInt(1, 1)
+                -Random.nextInt(1, distance / AMBIT)
             } else {
                 Random.nextInt(1, distance / AMBIT)
             }
@@ -86,31 +36,48 @@ class Map {
         return move
 
     }
-    fun moveCatsMap(cats: Array<Cat>, amountCats: Int, height: Int, weight: Int) {
-        val idCats = mixingCats(cats, amountCats)
-        
-        for (idCat in idCats) {
-            val currentCat = cats[idCat]
-            print("$currentCat ушел в ")
+    fun moveCats(cats: Array<Cat>, amountCats: Int, height: Int, weight: Int) {
+        for (cat in cats) {
+            print("$cat ушел в ")
 
-            currentCat.x = currentCat.x + move(weight)
-            currentCat.y = currentCat.y + move(height)
-            currentCat.status = Status.WALK
+            cat.x = cat.x + move(weight)
+            cat.y = cat.y + move(height)
+            cat.status = Status.WALK
 
-            if (currentCat.x >= weight) {
-                currentCat.x = currentCat.x - weight
-            } else if ((currentCat.x < 0)) {
-                currentCat.x = currentCat.x + weight
+            if (cat.x >= weight) {
+                cat.x = cat.x - weight
+            } else if ((cat.x < 0)) {
+                cat.x = cat.x + weight
             }
 
-
-            if (currentCat.y >= height) {
-                currentCat.y = currentCat.y - height
-            } else if ((currentCat.y < 0)) {
-                currentCat.y = currentCat.y + height
+            if (cat.y >= height) {
+                cat.y = cat.y - height
+            } else if ((cat.y < 0)) {
+                cat.y = cat.y + height
             }
 
-            println("x = ${currentCat.x}, y = ${currentCat.y}")
+            println("x = ${cat.x}, y = ${cat.y}")
+
+            for (fightCat in cat.getFightCats()) {
+                fightCat.removeFightCat(cat)
+
+                if (fightCat.getFightCats().isEmpty()) {
+                    fightCat.status = Status.WALK
+                }
+            }
+            cat.removeAllFightCats()
+
+            for (hissCat in cat.getHissCats()) {
+                hissCat.removeHissCat(cat)
+
+                if (hissCat.getHissCats().isEmpty() && hissCat.status != Status.FIGHT) {
+                    hissCat.status = Status.WALK
+                }
+            }
+            cat.removeAllHissCats()
         }
     }
 }
+
+
+
--- Program/src/main/kotlin/Status.kt
+++ Program/src/main/kotlin/Status.kt
@@ -1,111 +1,46 @@
 import kotlin.random.Random
 
-class UpdateStatus(private val catsMap: Array<Array<CatInMap>>, private val r0: Double, private val r1: Double, private val nameDistance: NameDistance = NameDistance.Euclidean) {
+class UpdateStatus(private val cats: Array<Cat>, private val r0: Double, private val r1: Double, private val nameDistance: NameDistance = NameDistance.Euclidean) {
 
-    fun updateStatus() {
-        check(r0, Status.FIGHT)
-        check(r1, Status.HISS)
+    init {
+        updateCatStatus()
     }
+    private fun updateCatStatus() {
+        for (i in cats.indices) {
+            for (j in i + 1 until cats.size) {
+                val distance = Distance(cats[i], cats[j]).euclideanDistance()
+                if (distance <= r0) {
+                    cats[i].status = Status.FIGHT
+                    cats[j].status = Status.FIGHT
 
-    private fun check(r: Double, status: Status) {
-        // Проходим по всем котам в catsMap
-        for (i in catsMap.indices) {
-            for (j in 0 until catsMap[i].size) {
-                // Проверяем, что текущий кот не {0, ...}
-                if (catsMap[i][j].number != 0) {
-                    val currentCat = catsMap[i][j].cat
-                    addStatus(r, currentCat, status)
+                    println("${cats[i]} дерется ${cats[j]}")
+
+                    cats[i].addFightCats(cats[j])
+                    cats[j].addFightCats(cats[i])
                 }
             }
         }
-    }
 
-    fun addStatus(r: Double, cat: Cat, status: Status) {
-        var leftStart = cat.x - r0.toInt() - 1
-        var rightStart = cat.x + r0.toInt() + 1
+        for (i in cats.indices) {
+            if (cats[i].status == Status.WALK) {
+                for (j in i + 1 until cats.size) {
+                    val distance = Distance(cats[i], cats[j]).euclideanDistance()
+                    if (distance <= r1 && cats[i].status != Status.FIGHT) {
+                        val probability = 1 / distance
+                        val randomValue = Random.nextDouble()
 
-        if (leftStart < 0) {
-            leftStart = 0
-        }
-        if (rightStart >= catsMap.size) {
-            rightStart = catsMap.size - 1
-        }
+                        if (randomValue <= probability) {
+                            cats[i].status = Status.HISS
+                            cats[i].addHiisCat(cats[j])
+                            cats[j].addHiisCat(cats[i])
+                            println("${cats[i]} шипит на ${cats[j]}")
 
-        var startY = cat.y - r0.toInt()
-        if (startY < 0) { startY = 0 }
-
-        for (y in startY until catsMap[leftStart].size) {
-            var c = -1
-            var x = leftStart
-            val right = rightStart
-
-            if (y >= catsMap[x].size) {
-                break
-            }
-
-            while (x <= right) {
-                val distance =
-                    when (nameDistance) {
-                        NameDistance.Euclidean ->
-                            Distance(catsMap[x][y].cat, cat).euclideanDistance()
-
-                        NameDistance.Manhattan ->
-                            Distance(catsMap[x][y].cat, cat).manhattanDistance()
-
-                        NameDistance.Chebyshev ->
-                            Distance(catsMap[x][y].cat, cat).chebyshevDistance()
-
-                        NameDistance.Curvilinear ->
-                            Distance(catsMap[x][y].cat, cat).curvilinearDistance()
+                            if (cats[j].status != Status.FIGHT) {
+                                cats[j].status = Status.HISS
+                                println("${cats[j]} шипит на $cats[i]")
+                            }
+                        }
                     }
-
-                // Проверяем, что не текущий кот и что расстояние не превышает r0
-                if (x == cat.x && y == cat.y || distance > r) {
-                    x++
-                    continue
-                }
-
-                // Добавляем статус в зависимости от типа
-                when (status) {
-                    Status.FIGHT -> checkFight(cat, x, y)
-                    Status.HISS -> checkHiss(cat, x, y, distance)
-                    else -> {
-                        println("WTF")
-                    }
-                }
-
-                c++
-                x++
-            }
-
-            if (c < 0) {
-                break
-            }
-        }
-    }
-
-    private fun checkFight(cat: Cat, x: Int, y: Int) {
-        if (catsMap[x][y].number != 0) {
-            catsMap[x][y].cat.status = Status.FIGHT
-            cat.status = Status.FIGHT
-            println("$cat дерется с ${catsMap[x][y].cat}")
-        }
-    }
-
-    private fun checkHiss(cat: Cat, x: Int, y: Int, distance: Float) {
-        if (catsMap[x][y].number != 0 && cat.status == Status.WALK) {
-            // Вероятность
-            val probability = 1 / distance
-
-            // Генерируем случайное число от 0 до 1
-            val randomValue = Random.nextDouble()
-
-            if (randomValue <= probability) {
-                cat.status = Status.HISS
-                println("$cat шипит на ${catsMap[x][y].cat}")
-                if (catsMap[x][y].cat.status != Status.FIGHT) {
-                    catsMap[x][y].cat.status = Status.HISS
-                    println("${catsMap[x][y].cat} шипит на $cat")
                 }
             }
         }
